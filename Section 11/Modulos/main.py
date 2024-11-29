463895ccbe77caec49197458f1542171ef0ef11c
JoseDFN <63378897+JoseDFN@users.noreply.github.com>
2024-11-29 11:02:46 -0500

    feat: cambio la estructura de los archivos y se agrega la seccion 11

---
+++ Section 11/Modulos/main.py
@@ -0,0 +1,9 @@
+import datetime as dt
+from datetime import date, datetime
+
+hoy = dt.datetime.now()
+hoy2 = date.today()
+ahora = datetime.now()
+
+print (f"Hoy es {hoy2} \nHoy es {hoy}")
+print (f"ahora es {ahora}")
